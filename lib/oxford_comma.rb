7256560a81e60f16b363d7931ae5ae00715f71fc
Will Saffran <willh.saffran@gmail.com>
2019-02-06 20:23:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  new_array = []
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2  
+    array.each.with_index(1) do |item, i|
+      if i < array.length
+        new_array << "#{item}, "
+      elsif i == array.length
+        new_array << "and #{item}"
+      end
+    end
+    new_array.join
+  end
 end
