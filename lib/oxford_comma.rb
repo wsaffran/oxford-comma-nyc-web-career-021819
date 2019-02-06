45b8b82890b2b5aa40529f1f6e67f9695c61bbc6
Will Saffran <willh.saffran@gmail.com>
2019-02-06 20:18:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,15 +4,12 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length > 2  
-  
-    array.each do |item|
+    array.each.with_index(1) do |item, i|
       if i < array.length
         array.join(", ")
-      
-      if i == array.length
+      elsif i == array.length
         array.join(", and ")
       end
-      
     end
   end
 end
