9a2652ca0736169758ad3843768d70db662f2c09
Will Saffran <willh.saffran@gmail.com>
2019-02-06 20:14:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,9 +5,9 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length > 2  
     array.each.with_index(1) do |item, i|
-      if i < array.length
+      if i < array.length - 1
         array.join(", ")
-      elsif i == array.length
+      elsif i == array.length - 1
         array.join(", and ")
       end
     end
